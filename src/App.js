20a0643b2ed466c2b7a058a1169d6f0ea3932fb8
ray <houimliraed@outlook.fr>
2024-02-05 00:38:03 +0200

    api key

--- src/App.js
+++ src/App.js
@@ -1,6 +1,15 @@
-import React from "react";
+import {React, useEffect} from "react";
+// API KET 9c67e612
 
-const App =() =>{
+const API_URL = "http://omdbapi.com/?apikey=9c67e612";
+
+const App = () =>{
+
+useEffect(()=>{
+
+},[])
+
+
  return (
   <p> Hello React</p>
  );
